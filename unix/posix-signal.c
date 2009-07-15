4999285cee9f1f00bc72a16607bbaf6d133c7483
Konstantin Khomoutov <kostix@frog.(none)>
2009-07-15 12:21:32 +0400

    Reformat sources to match Tcl core style
    unix/posix-signal.c modified:
    * Reformatted using indenting with 4 spaces and
      each 8 spaces being replaced with a tab.
    * Added Vim modeline to set this mode.

--- unix/posix-signal.c
+++ unix/posix-signal.c
@@ -4,103 +4,105 @@
 #define SIGDECL(SIG) { SIG, #SIG }
 
 const struct {
-	int signal;
-	char *name;
+    int signal;
+    char *name;
 } signals[] = {
-	SIGDECL(SIGHUP),
-	SIGDECL(SIGINT),
-	SIGDECL(SIGQUIT),
-	SIGDECL(SIGILL),
-	SIGDECL(SIGABRT),
-	SIGDECL(SIGFPE),
-	SIGDECL(SIGKILL),
-	SIGDECL(SIGSEGV),
-	SIGDECL(SIGPIPE),
-	SIGDECL(SIGALRM),
-	SIGDECL(SIGTERM),
-	SIGDECL(SIGUSR1),
-	SIGDECL(SIGUSR2),
-	SIGDECL(SIGCHLD),
-	SIGDECL(SIGCONT),
-	SIGDECL(SIGSTOP),
-	SIGDECL(SIGTSTP),
-	SIGDECL(SIGTTIN),
-	SIGDECL(SIGTTOU),
+    SIGDECL(SIGHUP),
+    SIGDECL(SIGINT),
+    SIGDECL(SIGQUIT),
+    SIGDECL(SIGILL),
+    SIGDECL(SIGABRT),
+    SIGDECL(SIGFPE),
+    SIGDECL(SIGKILL),
+    SIGDECL(SIGSEGV),
+    SIGDECL(SIGPIPE),
+    SIGDECL(SIGALRM),
+    SIGDECL(SIGTERM),
+    SIGDECL(SIGUSR1),
+    SIGDECL(SIGUSR2),
+    SIGDECL(SIGCHLD),
+    SIGDECL(SIGCONT),
+    SIGDECL(SIGSTOP),
+    SIGDECL(SIGTSTP),
+    SIGDECL(SIGTTIN),
+    SIGDECL(SIGTTOU),
 };
-
+
 static
 void
 InitSignalLookupTables (void) {
-	int i;
-	for (i = 0; i < sizeof(signals)/sizeof(signals[0]); ++i) {
-		printf("%s %d\n", signals[i].name, signals[i].signal);
-	}
+    int i;
+    for (i = 0; i < sizeof(signals)/sizeof(signals[0]); ++i) {
+	printf("%s %d\n", signals[i].name, signals[i].signal);
+    }
 }
-
+
 static
 int
 Command_Trap (
-	ClientData clientData,
-	Tcl_Interp *interp,
-	int objc,
-	Tcl_Obj *const objv[]
-	)
+    ClientData clientData,
+    Tcl_Interp *interp,
+    int objc,
+    Tcl_Obj *const objv[]
+    )
 {
-	return TCL_OK;
+    return TCL_OK;
 }
-
+
+static
 int
 Signal_Command (
-	ClientData clientData,
-	Tcl_Interp *interp,
-	int objc,
-	Tcl_Obj *const objv[]
+    ClientData clientData,
+    Tcl_Interp *interp,
+    int objc,
+    Tcl_Obj *const objv[]
 	)
 {
-	const char *cmds[] = { "trap", NULL };
-	Tcl_ObjCmdProc *const procs[] = {
-		Command_Trap
-	};
+    const char *cmds[] = { "trap", NULL };
+    Tcl_ObjCmdProc *const procs[] = {
+	Command_Trap
+    };
 
-	int cmd;
+    int cmd;
 
-	if (objc < 2) {
-		Tcl_WrongNumArgs(interp, 1, objv,
-			"subcommand ?arg ...?");
-		return TCL_ERROR;
-	}
+    if (objc < 2) {
+	Tcl_WrongNumArgs(interp, 1, objv,
+		"subcommand ?arg ...?");
+	    return TCL_ERROR;
+    }
 
-	if (Tcl_GetIndexFromObj(interp, objv[1],
-			cmds, "subcommand", 0, &cmd) != TCL_OK) {
-		return TCL_ERROR;
-	}
+    if (Tcl_GetIndexFromObj(interp, objv[1],
+	    cmds, "subcommand", 0, &cmd) != TCL_OK) {
+	return TCL_ERROR;
+    }
 
-	return procs[cmd](clientData, interp, objc, objv);
+    return procs[cmd](clientData, interp, objc, objv);
 }
-
-EXTERN int
+
+int
 Posixsignal_Init(Tcl_Interp * interp)
 {
-	ClientData clientData;
+    ClientData clientData;
 
 #ifdef USE_TCL_STUBS
-	if (Tcl_InitStubs(interp, "8.1", 0) == NULL) {
-		return TCL_ERROR;
-	}
+    if (Tcl_InitStubs(interp, "8.1", 0) == NULL) {
+	return TCL_ERROR;
+    }
 #endif
-	if (Tcl_PkgRequire(interp, "Tcl", "8.1", 0) == NULL) {
-		return TCL_ERROR;
-	}
+    if (Tcl_PkgRequire(interp, "Tcl", "8.1", 0) == NULL) {
+	return TCL_ERROR;
+    }
 
-	InitSignalLookupTables();
+    InitSignalLookupTables();
 
-	Tcl_CreateObjCommand(interp, PACKAGE_NAME,
-			Signal_Command, clientData, NULL);
+    Tcl_CreateObjCommand(interp, PACKAGE_NAME,
+	    Signal_Command, clientData, NULL);
 
-	if (Tcl_PkgProvide(interp, PACKAGE_NAME, PACKAGE_VERSION) != TCL_OK) {
-		return TCL_ERROR;
-	}
+    if (Tcl_PkgProvide(interp, PACKAGE_NAME, PACKAGE_VERSION) != TCL_OK) {
+	return TCL_ERROR;
+    }
 
-	return TCL_OK;
+    return TCL_OK;
 }
 
+/* vim: set ts=8 sts=4 sw=4 sts=4 noet: */
