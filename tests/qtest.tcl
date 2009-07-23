7d62ac34b60096bb000854b27828e5069a49888d
Konstantin Khomoutov <kostix@jukebox.domain007.com>
2009-07-23 21:18:05 +0400

    Add simple test script
    Added a simple test script that listens to SIGHUP.

---
+++ tests/qtest.tcl
@@ -0,0 +1,13 @@
+load [file join [file dir [info script]] .. libposixsignal0.1.so]
+
+proc keepalive {msec} {
+	puts alive
+	after $msec [list keepalive $msec]
+}
+
+posix::signal trap SIGHUP {puts SIGHUP}
+
+#keepalive 3000
+
+vwait forever
+
