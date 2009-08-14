a3ccd4b0f43a92869a1a574f9f61af1b4b6db588
Konstantin Khomoutov <kostix@frog>
2009-08-14 11:07:30 +0400

    Add more functions to sigcont
    Added functions to create and delete an element,
    iterate over and free the container.

--- unix/sigcont.c
+++ unix/sigcont.c
@@ -6,7 +6,7 @@
 
 typedef Tcl_HashTable SigContainer;
 
-typedef Tcl_HashEntry SigContEntry;
+typedef Tcl_HashSearch SigContSearch;
 
 void
 SigCont_Init (
@@ -16,13 +16,83 @@ SigCont_Init (
     Tcl_InitHashTable(sigcontPtr, TCL_ONE_WORD_KEYS);
 }
 
-SigContEntry *
-SigCont_FindEntry (
+void
+SigCont_Free (
+    SigContainer *sigcontPtr
+    )
+{
+    Tcl_DeleteHashTable(sigcontPtr);
+}
+
+ClientData
+SigCont_GetEntry (
     SigContainer *sigcontPtr,
     int signum
     )
 {
-    return Tcl_FindHashEntry(sigcontPtr, WORDKEY(signum));
+    Tcl_HashEntry *entryPtr;
+
+    entryPtr = Tcl_FindHashEntry(sigcontPtr, WORDKEY(signum));
+    if (entryPtr != NULL) {
+	return Tcl_GetHashValue(entryPtr);
+    } else {
+	return NULL;
+    }
+}
+
+int
+SigCont_CreateEntry (
+    SigContainer *sigcontPtr,
+    int signum,
+    ClientData clientData
+    )
+{
+    Tcl_HashEntry *entryPtr;
+    int isnew, ok;
+
+    entryPtr = Tcl_CreateHashEntry(sigcontPtr, WORDKEY(signum), &isnew);
+    ok = !isnew;
+    if (ok) {
+	Tcl_SetHashValue(entryPtr, clientData);
+    }
+    return ok;
+}
+
+ClientData
+SigCont_DeleteEntry (
+    SigContainer *sigcontPtr,
+    int signum
+    )
+{
+    Tcl_HashEntry *entryPtr;
+
+    entryPtr = Tcl_FindHashEntry(sigcontPtr, WORDKEY(signum));
+    if (entryPtr != NULL) {
+	ClientData clientData;
+
+	clientData = Tcl_GetHashValue(entryPtr);
+	Tcl_DeleteHashEntry(entryPtr);
+	return clientData;
+    } else {
+	return NULL;
+    }
+}
+
+ClientData
+SigCont_FirstEntry (
+    SigContainer *sigcontPtr,
+    SigContSearch *searchPtr
+    )
+{
+    return Tcl_FirstHashEntry(sigcontPtr, searchPtr);
+}
+
+ClientData
+SigCont_NextEntry (
+    SigContSearch *searchPtr
+    )
+{
+    return Tcl_NextHashEntry(searchPtr);
 }
 
 /* vim: set ts=8 sts=4 sw=4 sts=4 noet: */
